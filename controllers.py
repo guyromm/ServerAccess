31bba7dc6f5f850ae09495dd20bd40be64c32991
Guy Romm <guyromm@gmail.com>
2012-01-01 18:25:58 +0200

    apache integration

--- controllers.py
+++ controllers.py
@@ -5,7 +5,7 @@ filedesc: default controller file
 from noodles.http import Response
 from noodles.templates import render_to_response
 from commands import getstatusoutput as gso
-from config import PW_FILE,IPT_CHAIN,DEFAULT_ADMIN,IPT_INSPOS
+from config import PW_FILE,IPT_CHAIN,DEFAULT_ADMIN,IPT_INSPOS,DIGEST_ZONE
 import re
 
 #insert:
@@ -37,13 +37,22 @@ def get_fw_rules(users=None):
             source = rule_params.group('source')
             user = res.group(1)
             if users and user not in users: raise Exception('unknown user %s'%user)
-            if user not in rt: rt[user]=[]
+            if user not in rt: 
+                #raise Exception('adding user %s because not in %s'%(user,rt.keys()))
+                rt[user]=[]
             rt[user].append({'source':source,'cnt':cnt})
             all_allowed.append(source)
     return rt,all_allowed
 def get_users():
-    u = [rw.split(':')[0] for rw in open(PW_FILE,'r').read().split('\n')]
-    return u
+    fconts = open(PW_FILE,'r').read()
+    rt=[]
+    for rw in fconts.split('\n'):
+        spl = rw.split(':')
+        if len(spl)<2: continue
+        #print 'going over %s'%spl
+        if spl[1]==DIGEST_ZONE:
+            rt.append(spl[0])
+    return rt
 def allow_access(user,ip):
     users = get_users()
     rules,all_allowed = get_fw_rules(users)
@@ -51,7 +60,9 @@ def allow_access(user,ip):
         cmd = 'sudo iptables -IINPUT %s -s %s -j ACCEPT -m comment --comment="ServerAccess user:%s"'%(IPT_INSPOS,ip,user)
         st,op=gso(cmd) ; assert st==0
     
-def revoke_access(user,ip,cnt):
+def revoke_access(user,ip,cnt,op_user,is_admin):
+    #we allow admin to op on all |  or user to operate on himself
+    if not (is_admin or user==op_user): raise Exception('auth violation')
     users = get_users()
     r,aips = get_fw_rules(users)
     assert '.' in ip
@@ -64,9 +75,13 @@ def revoke_access(user,ip,cnt):
 
     pass
 def get_admin(r,d):
+    username = re.compile('username="([^"]+)"').search(r.headers.get('Authorization'))
+    if username:
+        return username.group(1)
     return d
 def index(request):
     admin = get_admin(request,DEFAULT_ADMIN)
+    is_admin = (admin==DEFAULT_ADMIN)
     if request.method=='POST':
         aip = request.params.get('add-ip')
         if request.params.get('add-ip-btn'):
@@ -77,7 +92,7 @@ def index(request):
                 user = spl[1]
                 cnt = spl[2]
                 ip = spl[3]
-                revoke_access(user,ip,cnt)
+                revoke_access(user,ip,cnt,admin,is_admin)
     users = get_users()
     rules,all_allowed = get_fw_rules(users)
     for u in users: 
@@ -86,10 +101,11 @@ def index(request):
         #show all users' rules
         pass
     else:
-        for k in rules:
+        for k in rules.keys():
             if k!=admin: 
                 del rules[k]
+        users = [admin]
 
-    c = {'users':users,'rules':rules,'remote_ip':request.remote_addr,'is_admin':admin==DEFAULT_ADMIN,'user':admin}
+    c = {'users':users,'rules':rules,'remote_ip':request.remote_addr,'is_admin':is_admin,'user':admin}
     return render_to_response('index.html',c)
     #return Response('<h1>Hello, NoodlesFramework!</h1>')
